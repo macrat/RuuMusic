5a06b630793ebe6ec6fe745daedea675f49b3bd6
MacRat <m@crat.jp>
2015-11-20 10:10:00 +0900

    Removed unused import

--- app/src/main/java/jp/blanktar/ruumusic/client/PlaylistFragment.java
+++ app/src/main/java/jp/blanktar/ruumusic/client/PlaylistFragment.java
@@ -14,7 +14,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
