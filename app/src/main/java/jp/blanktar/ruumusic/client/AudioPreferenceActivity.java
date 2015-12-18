b650c5486387b3d998d3e7115e7371fc97af80c5
MacRat <m@crat.jp>
2015-12-18 13:08:43 +0900

    Added necessary import I was wrong delete

--- app/src/main/java/jp/blanktar/ruumusic/client/AudioPreferenceActivity.java
+++ app/src/main/java/jp/blanktar/ruumusic/client/AudioPreferenceActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.media.audiofx.PresetReverb;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
