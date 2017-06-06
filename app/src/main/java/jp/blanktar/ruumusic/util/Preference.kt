1479e7f65360148137bfb65bd778f4410bc17bc1
MacRat <m@crat.jp>
2017-06-07 01:58:14 +0900

    Removed unnecessary variable

--- app/src/main/java/jp/blanktar/ruumusic/util/Preference.kt
+++ app/src/main/java/jp/blanktar/ruumusic/util/Preference.kt
@@ -17,36 +17,36 @@ enum class RepeatModeType {
 class Preference(val context: Context) {
     @JvmField val AudioPrefix = "audio_"
 
-    @JvmField val RootDirectory = StringPreferenceHandler(context, "root_directory")
+    @JvmField val RootDirectory = StringPreferenceHandler("root_directory")
 
-    @JvmField val BassBoostEnabled = BooleanPreferenceHandler(context, AudioPrefix + "bassboost_enabled")
-    @JvmField val BassBoostLevel = ShortPreferenceHandler(context, AudioPrefix + "bassboost_level")
+    @JvmField val BassBoostEnabled = BooleanPreferenceHandler(AudioPrefix + "bassboost_enabled")
+    @JvmField val BassBoostLevel = ShortPreferenceHandler(AudioPrefix + "bassboost_level")
 
-    @JvmField val ReverbEnabled = BooleanPreferenceHandler(context, AudioPrefix + "reverb_enabled")
-    @JvmField val ReverbType = ShortPreferenceHandler(context, AudioPrefix + "reverb_type")
+    @JvmField val ReverbEnabled = BooleanPreferenceHandler(AudioPrefix + "reverb_enabled")
+    @JvmField val ReverbType = ShortPreferenceHandler(AudioPrefix + "reverb_type")
 
-    @JvmField val LoudnessEnabled = BooleanPreferenceHandler(context, AudioPrefix + "loudness_enabled")
-    @JvmField val LoudnessLevel = IntPreferenceHandler(context, AudioPrefix + "loudness_level")
+    @JvmField val LoudnessEnabled = BooleanPreferenceHandler(AudioPrefix + "loudness_enabled")
+    @JvmField val LoudnessLevel = IntPreferenceHandler(AudioPrefix + "loudness_level")
 
-    @JvmField val EqualizerEnabled = BooleanPreferenceHandler(context, AudioPrefix + "equalizer_enabled")
-    @JvmField val EqualizerPreset = ShortPreferenceHandler(context, AudioPrefix + "equalizer_preset", default = -1)
-    @JvmField val EqualizerLevel = IntListPreferenceHandler(context, AudioPrefix + "equalizer_level")
+    @JvmField val EqualizerEnabled = BooleanPreferenceHandler(AudioPrefix + "equalizer_enabled")
+    @JvmField val EqualizerPreset = ShortPreferenceHandler(AudioPrefix + "equalizer_preset", default = -1)
+    @JvmField val EqualizerLevel = IntListPreferenceHandler(AudioPrefix + "equalizer_level")
 
     // client state
-    @JvmField val LastViewPage = IntPreferenceHandler(context, "last_view_page", 1)
+    @JvmField val LastViewPage = IntPreferenceHandler("last_view_page", 1)
 
     // player state
-    @JvmField val RepeatMode = EnumPreferenceHandler<RepeatModeType>(context, "repeat_mode", RepeatModeType.OFF, {x -> RepeatModeType.valueOf(x)})
-    @JvmField val ShuffleMode = BooleanPreferenceHandler(context, "shuffle_mode")
-    @JvmField val RecursivePath = StringPreferenceHandler(context, "recursive_path")
-    @JvmField val SearchQuery = StringPreferenceHandler(context, "search_query")
-    @JvmField val SearchPath = StringPreferenceHandler(context, "search_path")
-    @JvmField val LastPlayMusic = StringPreferenceHandler(context, "last_play_music")
-    @JvmField val LastPlayPosition = IntPreferenceHandler(context, "last_play_position")
+    @JvmField val RepeatMode = EnumPreferenceHandler<RepeatModeType>("repeat_mode", RepeatModeType.OFF, {x -> RepeatModeType.valueOf(x)})
+    @JvmField val ShuffleMode = BooleanPreferenceHandler("shuffle_mode")
+    @JvmField val RecursivePath = StringPreferenceHandler("recursive_path")
+    @JvmField val SearchQuery = StringPreferenceHandler("search_query")
+    @JvmField val SearchPath = StringPreferenceHandler("search_path")
+    @JvmField val LastPlayMusic = StringPreferenceHandler("last_play_music")
+    @JvmField val LastPlayPosition = IntPreferenceHandler("last_play_position")
 
     // playlist state
-    @JvmField val CurrentViewPath = StringPreferenceHandler(context, "current_view_path")
-    @JvmField val LastSearchQuery = StringPreferenceHandler(context, "last_search_query")
+    @JvmField val CurrentViewPath = StringPreferenceHandler("current_view_path")
+    @JvmField val LastSearchQuery = StringPreferenceHandler("last_search_query")
 
 
     val listeners = mutableSetOf<PreferenceHandler<*>>()
@@ -63,7 +63,7 @@ class Preference(val context: Context) {
         }
     }
 
-    abstract inner class PreferenceHandler<T>(val context: Context, val key: String, val default: T) : SharedPreferences.OnSharedPreferenceChangeListener {
+    abstract inner class PreferenceHandler<T>(val key: String, val default: T) : SharedPreferences.OnSharedPreferenceChangeListener {
         var receiver: (() -> Unit)? = null
 
         val sharedPreferences: SharedPreferences
@@ -100,7 +100,7 @@ class Preference(val context: Context) {
     }
 
 
-    inner class IntPreferenceHandler(context: Context, key: String, default: Int = 0) : PreferenceHandler<Int>(context, key, default) {
+    inner class IntPreferenceHandler(key: String, default: Int = 0) : PreferenceHandler<Int>(key, default) {
         override fun get() = sharedPreferences.getInt(key, default)
 
         override fun set(value: Int) {
@@ -109,7 +109,7 @@ class Preference(val context: Context) {
     }
 
 
-    inner class ShortPreferenceHandler(context: Context, key: String, default: Short = 0) : PreferenceHandler<Short>(context, key, default) {
+    inner class ShortPreferenceHandler(key: String, default: Short = 0) : PreferenceHandler<Short>(key, default) {
         override fun get() = sharedPreferences.getInt(key, default.toInt()).toShort()
 
         override fun set(value: Short) {
@@ -118,7 +118,7 @@ class Preference(val context: Context) {
     }
 
 
-    inner class IntListPreferenceHandler(context: Context, key: String, val defaultInt: Int = 0) : PreferenceHandler<List<Int>>(context, key, listOf<Int>()) {
+    inner class IntListPreferenceHandler(key: String, val defaultInt: Int = 0) : PreferenceHandler<List<Int>>(key, listOf<Int>()) {
         private fun keyOf(index: Int) = "%s_%d".format(key, index)
 
         fun get(index: Int) = sharedPreferences.getInt(keyOf(index), defaultInt)
@@ -155,7 +155,7 @@ class Preference(val context: Context) {
     }
 
 
-    inner class BooleanPreferenceHandler(context: Context, key: String, default: Boolean = false) : PreferenceHandler<Boolean>(context, key, default) {
+    inner class BooleanPreferenceHandler(key: String, default: Boolean = false) : PreferenceHandler<Boolean>(key, default) {
         override fun get() = sharedPreferences.getBoolean(key, default)
 
         override fun set(value: Boolean) {
@@ -164,7 +164,7 @@ class Preference(val context: Context) {
     }
 
 
-    inner class StringPreferenceHandler(context: Context, key: String, default: String? = null) : PreferenceHandler<String?>(context, key, default) {
+    inner class StringPreferenceHandler(key: String, default: String? = null) : PreferenceHandler<String?>(key, default) {
         override fun get() = sharedPreferences.getString(key, default)
 
         override fun set(value: String?) {
@@ -172,7 +172,7 @@ class Preference(val context: Context) {
         }
     }
 
-    inner class EnumPreferenceHandler<T: Enum<T>>(context: Context, key: String, default: T, val asEnum: (String) -> T) : PreferenceHandler<T>(context, key, default) {
+    inner class EnumPreferenceHandler<T: Enum<T>>(key: String, default: T, val asEnum: (String) -> T) : PreferenceHandler<T>(key, default) {
         override fun get(): T {
             try {
                 return asEnum(sharedPreferences.getString(key, ""))
