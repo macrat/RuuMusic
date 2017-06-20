931bbcd5ed6ff96bc048535fd1b158eb964f23bc
MacRat <m@crat.jp>
2017-06-20 22:34:17 +0900

    Removed unused import

--- app/src/main/java/jp/blanktar/ruumusic/service/RuuService.java
+++ app/src/main/java/jp/blanktar/ruumusic/service/RuuService.java
@@ -26,7 +26,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v4.media.MediaBrowserCompat.MediaItem;
 import android.support.v4.media.MediaBrowserServiceCompat;
