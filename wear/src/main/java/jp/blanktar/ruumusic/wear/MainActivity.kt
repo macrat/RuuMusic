17139a029713869a4f06e716745db11fb84e0313
MacRat <m@crat.jp>
2017-06-26 18:09:47 +0900

    Added Wear module

---
+++ wear/src/main/java/jp/blanktar/ruumusic/wear/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.blanktar.ruumusic.wear
+
+import android.app.Activity
+import android.os.Bundle
+
+
+class MainActivity : Activity() {
+    override protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
