e8393d1ddd99c98f4872cbd607b5e82012460ded
MacRat <m@crat.jp>
2017-06-27 20:36:33 +0900

    Added player fragment design for wear

--- wear/src/main/java/jp/blanktar/ruumusic/wear/MainActivity.kt
+++ wear/src/main/java/jp/blanktar/ruumusic/wear/MainActivity.kt
@@ -2,11 +2,35 @@ package jp.blanktar.ruumusic.wear
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v13.app.FragmentPagerAdapter
+import android.app.FragmentManager
+import android.support.v4.view.ViewPager
+import android.app.Fragment
 
 
 class MainActivity : Activity() {
     override protected fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pagerAdapter = PagerAdapter(getFragmentManager())
+        (findViewById(R.id.pager) as ViewPager).setAdapter(pagerAdapter)
+    }
+
+    inner class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment? {
+            when (position) {
+                0 -> return PlayerFragment()
+            }
+            return null
+        }
+
+        override fun getCount(): Int {
+            return 1
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return "Player"
+        }
     }
 }
---
+++ wear/src/main/java/jp/blanktar/ruumusic/wear/PlayerFragment.kt
@@ -0,0 +1,19 @@
+package jp.blanktar.ruumusic.wear
+
+import android.os.Bundle
+import android.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class PlayerFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_player, container, false)
+    }
+}
