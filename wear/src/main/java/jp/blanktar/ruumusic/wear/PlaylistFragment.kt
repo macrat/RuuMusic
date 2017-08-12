08ef24c3fe91bb2bfe7b8edfa72bd8199f7533c0
MacRat <m@crat.jp>
2017-08-12 17:22:11 +0900

    Fixed bug that couldn't start wear app

--- wear/src/main/java/jp/blanktar/ruumusic/wear/PlaylistFragment.kt
+++ wear/src/main/java/jp/blanktar/ruumusic/wear/PlaylistFragment.kt
@@ -12,25 +12,21 @@ import kotlinx.android.synthetic.main.fragment_playlist.*
 
 
 class PlaylistFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.fragment_playlist, container, false)
-        
+        return inflater!!.inflate(R.layout.fragment_playlist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         list.layoutManager = CurvedChildLayoutManager(getContext())
 
         list.adapter = RecyclerAdapter(arrayOf<String>("/this/is/test/",
-                                                       "/path/to/song/",
-                                                       "/this/is/test/",
-                                                       "/path/to/song/",
-                                                       "this is song",
-                                                       "song file name",
-                                                       "this is song",
-                                                       "song file name"))
-
-        return view
+                "/path/to/song/",
+                "/this/is/test/",
+                "/path/to/song/",
+                "this is song",
+                "song file name",
+                "this is song",
+                "song file name"))
     }
 }
