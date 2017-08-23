b3fdbeada07d2371b28ff7f67f592000e64593a4
MacRat <m@crat.jp>
2017-08-23 20:51:02 +0900

    Fixed bug that equalized was not applied theme

--- app/src/main/java/jp/blanktar/ruumusic/client/preference/SoundPreferenceActivity.kt
+++ app/src/main/java/jp/blanktar/ruumusic/client/preference/SoundPreferenceActivity.kt
@@ -6,15 +6,21 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.SeekBar
+import android.widget.AdapterView
 
 import jp.blanktar.ruumusic.R
 import jp.blanktar.ruumusic.util.EqualizerInfo
 import jp.blanktar.ruumusic.util.RuuClient
 import kotlinx.android.synthetic.main.activity_sound_preference.*
+import jp.blanktar.ruumusic.util.Preference
 
 
 class SoundPreferenceActivity : AppCompatActivity() {
-    var preference: jp.blanktar.ruumusic.util.Preference? = null
+    var preference: Preference? = null
     var client: RuuClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +30,7 @@ class SoundPreferenceActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        preference = jp.blanktar.ruumusic.util.Preference(applicationContext)
+        preference = Preference(applicationContext)
         client = RuuClient(applicationContext)
 
         setupBassBoost()
@@ -65,8 +71,8 @@ class SoundPreferenceActivity : AppCompatActivity() {
             e -> reverb_spinner.setEnabled(e)
         }
 
-        val adapter = ArrayAdapter.createFromResource(applicationContext, jp.blanktar.ruumusic.R.array.reverb_options, jp.blanktar.ruumusic.R.layout.spinner_item)
-        adapter.setDropDownViewResource(jp.blanktar.ruumusic.R.layout.spinner_dropdown_item)
+        val adapter = ArrayAdapter.createFromResource(applicationContext, R.array.reverb_options, R.layout.spinner_item)
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
 
         reverb_spinner.adapter = adapter
 
@@ -79,7 +85,7 @@ class SoundPreferenceActivity : AppCompatActivity() {
 
     private fun setupLoudness() {
         if (android.os.Build.VERSION.SDK_INT < 19) {
-            loudness_wrapper.setVisibility(android.view.View.GONE)
+            loudness_wrapper.setVisibility(View.GONE)
             return
         }
 
@@ -93,48 +99,49 @@ class SoundPreferenceActivity : AppCompatActivity() {
         val adapter = ArrayAdapter<String>(applicationContext, R.layout.spinner_item)
         adapter.add("Custom")
         info.presets.forEach { x -> adapter.add(x) }
-        adapter.setDropDownViewResource(jp.blanktar.ruumusic.R.layout.spinner_dropdown_item)
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
 
         equalizer_spinner.adapter = adapter
 
         equalizer_spinner.setSelection(preference!!.EqualizerPreset.get() + 1)
 
-        equalizer_spinner.onItemSelectedListener = object : android.widget.AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: android.widget.AdapterView<*>, view: android.view.View, position: Int, id: Long) {
+        equalizer_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 preference!!.EqualizerPreset.set((position - 1).toShort())
             }
 
-            override fun onNothingSelected(parent: android.widget.AdapterView<*>) {}
+            override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
         preference!!.EqualizerPreset.setOnChangeListener {
             equalizer_spinner.setSelection(preference!!.EqualizerPreset.get() + 1)
         }
 
-        val texts = mutableListOf<android.widget.TextView>()
-        val bars = mutableListOf<android.widget.SeekBar>()
+        val texts = mutableListOf<TextView>()
+        val bars = mutableListOf<SeekBar>()
 
+        val inflater = LayoutInflater.from(applicationContext)
         for (i in 0..(info.freqs.size-1)) {
-            val table = View.inflate(applicationContext, jp.blanktar.ruumusic.R.layout.equalizer_preference_row, equalizer_container) as android.view.ViewGroup
-            val row = table.getChildAt(table.getChildCount() - 1) as android.view.ViewGroup
+            val table = inflater.inflate(R.layout.equalizer_preference_row, equalizer_container) as ViewGroup
+            val row = table.getChildAt(table.getChildCount() - 1)
 
-            row.findViewById<android.widget.TextView>(jp.blanktar.ruumusic.R.id.equalizer_freq).setText("${info.freqs[i]/1000}Hz")
-            texts.add(row.findViewById<android.widget.TextView>(jp.blanktar.ruumusic.R.id.equalizer_freq))
+            row.findViewById<TextView>(R.id.equalizer_freq).setText("${info.freqs[i]/1000}Hz")
+            texts.add(row.findViewById<TextView>(R.id.equalizer_freq))
 
-            val seekBar = row.findViewById<android.widget.SeekBar>(jp.blanktar.ruumusic.R.id.equalizer_bar)
+            val seekBar = row.findViewById<SeekBar>(R.id.equalizer_bar)
             seekBar.setMax(info.max - info.min)
             seekBar.setProgress(preference!!.EqualizerLevel.get(i) - info.min)
 
-            seekBar.setOnSeekBarChangeListener(object : android.widget.SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(b: android.widget.SeekBar, progress: Int, fromUser: Boolean){
+            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(b: SeekBar, progress: Int, fromUser: Boolean){
                     if (fromUser) {
                         preference!!.EqualizerPreset.set((-1).toShort())
                     }
                     preference!!.EqualizerLevel.set(i, progress + info.min)
                 }
 
-                override fun onStartTrackingTouch(b: android.widget.SeekBar) {}
-                override fun onStopTrackingTouch(b: android.widget.SeekBar) {}
+                override fun onStartTrackingTouch(b: SeekBar) {}
+                override fun onStopTrackingTouch(b: SeekBar) {}
             })
 
             bars.add(seekBar)
