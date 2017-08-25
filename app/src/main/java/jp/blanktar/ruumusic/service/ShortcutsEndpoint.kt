f50b6bab28325ece1d7e877e935fe53be6d13be3
MacRat <m@crat.jp>
2017-08-26 06:00:18 +0900

    Optimized ShortcutsEndpoint for under API level 25

--- app/src/main/java/jp/blanktar/ruumusic/service/ShortcutsEndpoint.kt
+++ app/src/main/java/jp/blanktar/ruumusic/service/ShortcutsEndpoint.kt
@@ -4,51 +4,52 @@ package jp.blanktar.ruumusic.service
 import kotlin.concurrent.thread
 
 import android.content.Context
+import android.os.Build
 
 import jp.blanktar.ruumusic.util.DynamicShortcuts
-import jp.blanktar.ruumusic.util.createDynamicShortcutInfo
 import jp.blanktar.ruumusic.util.EqualizerInfo
 import jp.blanktar.ruumusic.util.PlayingStatus
 import jp.blanktar.ruumusic.util.Preference
 import jp.blanktar.ruumusic.util.RuuFile
+import jp.blanktar.ruumusic.util.createDynamicShortcutInfo
 
 
 class ShortcutsEndpoint(val context: Context) : Endpoint {
-    val manager = DynamicShortcuts(context)
-    val preference = Preference(context)
+    val manager = if (Build.VERSION.SDK_INT >= 25) DynamicShortcuts(context) else null
+    val preference = if (Build.VERSION.SDK_INT >= 25) Preference(context) else null
 
     init {
-        preference.RootDirectory.setOnChangeListener {
+        preference?.RootDirectory?.setOnChangeListener {
             thread {
-                manager.managePinnedShortcuts()
+                manager!!.managePinnedShortcuts()
             }
         }
     }
 
     override fun close() {
-        preference.unsetAllListeners()
+        preference?.unsetAllListeners()
     }
 
     override fun onStatusUpdated(status: PlayingStatus) {
-        if (status.currentMusic == null) {
+        if (Build.VERSION.SDK_INT < 25 || status.currentMusic == null) {
             return
         }
 
-        val shortcuts = preference.ListedDynamicShortcuts.get().filter { it != status.currentMusic.parent }.toMutableList()
+        val shortcuts = preference!!.ListedDynamicShortcuts.get().filter { it != status.currentMusic.parent }.toMutableList()
         shortcuts.add(status.currentMusic.parent)
 
-        manager.shortcuts = shortcuts.map {
+        manager!!.shortcuts = shortcuts.map {
             createDynamicShortcutInfo(context, it)
         }.filterNotNull()
 
-        preference.ListedDynamicShortcuts.set(shortcuts)
+        preference!!.ListedDynamicShortcuts.set(shortcuts)
     }
 
     override fun onEqualizerInfo(info: EqualizerInfo) {}
 
     override fun onMediaStoreUpdated() {
         thread {
-            manager.managePinnedShortcuts()
+            manager!!.managePinnedShortcuts()
         }
     }
 
