2f3fd23b7b7b9178727842db0ac86d1dfa0ef520
MacRat <m@crat.jp>
2017-08-27 21:11:18 +0900

    Fixed bug that permission manager doesn't work well in old android

--- app/src/main/java/jp/blanktar/ruumusic/util/PermissionManager.kt
+++ app/src/main/java/jp/blanktar/ruumusic/util/PermissionManager.kt
@@ -1,16 +1,17 @@
 package jp.blanktar.ruumusic.util
 
 
-import android.support.v7.app.AppCompatActivity
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 
 
 class PermissionManager(val activity: Activity) {
     val hasPermission
-        get() = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        get() = Build.VERSION.SDK_INT < 23 || ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
 
     var onResultListener: OnResultListener? = null
 
